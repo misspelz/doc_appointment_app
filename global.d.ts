108a182e55ac051588991948509d78cd90c70950
misspelz <pelumiiadetoye@gmail.com>
2024-04-12 00:35:50 +0100

    fix

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,6 @@
 namespace NodeJS {
   interface ProcessEnv {
     HOST_NAME: string;
+    PROTOCOL: string;
   }
 }
--- next.config.js
+++ next.config.js
@@ -2,12 +2,31 @@
 
 // config.js
 const HOST_NAME = process.env.NEXT_PUBLIC_CLOUDINARY;
+const PROTOCOL = process.env.NEXT_PUBLIC_PROTOCOL;
 
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${PROTOCOL}://${HOST_NAME}/api/:path*`,
+      },
+      {
+        source: "/uploads/:path*",
+        destination: `${PROTOCOL}://${HOST_NAME}/uploads/:path*`,
+      },
+      {
+        source: "/_next/:path*",
+        destination: `/_next/:path*`,
+      },
+    ];
+  },
+
   images: {
     remotePatterns: [
       {
         hostname: `${HOST_NAME}`,
+        protocol: `${PROTOCOL}`,
       },
     ],
   },
