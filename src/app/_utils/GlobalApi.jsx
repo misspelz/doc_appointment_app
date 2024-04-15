7b8d66f59392ba29330c8532a5f96800475399f2
misspelz <pelumiiadetoye@gmail.com>
2024-04-15 10:54:54 +0100

    deployed strapi to cloud and added the deployment link

--- src/app/_utils/GlobalApi.jsx
+++ src/app/_utils/GlobalApi.jsx
@@ -3,7 +3,8 @@ const { default: axios } = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api",
+  // baseURL: "http://localhost:1337/api",
+  baseURL: "https://colorful-activity-3e4958dd98.strapiapp.com/api",
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
